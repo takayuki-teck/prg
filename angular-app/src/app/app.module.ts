2f59c1c9e43c40d561f58d9ed0d0483903f44096
takayuki-teck <horiuchi.takayuki66@gmail.com>
2021-08-31 15:47:13 +0900

    プロジェクトを作成しなおし

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
-  // bootstrap: [AppComponent]
-  bootstrap: [HelloComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
