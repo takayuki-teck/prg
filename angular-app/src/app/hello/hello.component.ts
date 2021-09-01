bff39fbb8d62d165267ff3e66ceeff63f9eb8cc9
takayuki-teck <horiuchi.takayuki66@gmail.com>
2021-09-01 22:41:35 +0900

    Angular超入門 2-3まで完了

--- angular-app/src/app/hello/hello.component.ts
+++ angular-app/src/app/hello/hello.component.ts
@@ -10,6 +10,10 @@ export class HelloComponent implements OnInit {
   message:string = '';
   now:Date = new Date();
   styleClass:string = '';
+  count:number = 0;
+  input:string = '';
+  visible:boolean = true;
+  data:string[] = [];
 
   constructor() {
     setInterval(
@@ -24,10 +28,25 @@ export class HelloComponent implements OnInit {
     this.title = 'Hello-app';
     this.message = 'This is My First Component!!';
     this.styleClass = 'red';
+    this.data = [
+      '最初の項目です',
+      '2番目の項目です',
+      '最後の項目です'
+    ]
   }
 
   today() {
     return this.now.toLocaleString();
+  }
+
+  doClick() {
+    this.message = ++this.count + "回、クリックしました。"
+    this.visible = !this.visible;
+  }
+
+  doType(val:string) {
+    this.input = val;
+    this.message = 'you type: ' + this.input;
   }
 
 }
