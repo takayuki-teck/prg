2ac35ee3e831774bb1ed48c97f5150a6cfec2268
takayuki <horiuchi.takayuki66@gmail.com>
2022-04-24 07:17:46 +0900

    commit

--- html/sample/js/script.js
+++ html/sample/js/script.js
@@ -1,6 +1,6 @@
 let my_botton = document.getElementById('my-botton');
 
 my_botton.addEventListener('click', (e) => {
-    e.preventDefault();
+    e.preventDefault(); // デフォルトの動作をキャンセルする
     alert('クリックされました!')
 });
